46c2a914d667344ca2c05e99a75349f4fe5e482c
David Amhed Beltrán Rubín de Celis <davikin_01@hotmail.com>
2023-10-28 20:31:06 -0300

    Agregando mainUTN para la UTN, archivo de prácticas

---
+++ JavaScript/scripts/mainUTN.js
@@ -0,0 +1,12 @@
+var nombreDelaVariableVar
+let nombreDeVariableLet
+const NOMBREVARIABLE = 'Curso de JavaScript'
+
+nombreDelaVariableVar = 'Pedro';
+nombreDelaVariableVar = 'Ricardo';
+nombreDelaVariableVar = 'Lucia';
+nombreDelaVariableVar = 'maria';
+
+alert(nombreDelaVariableVar);
+
+console.log(NOMBREVARIABLE);
