3c6143d130dd85bcfb71ec404a4a7591acb1e7fc
David Amhed Beltrán Rubín de Celis <davikin_01@hotmail.com>
2023-10-28 21:29:35 -0300

    Agregando información a constantes

--- JavaScript/scripts/mainUTN.js
+++ JavaScript/scripts/mainUTN.js
@@ -1,3 +1,5 @@
+// Declaraciones y asignaciones
+/*
 var nombreDelaVariableVar
 let nombreDeVariableLet
 const NOMBREVARIABLE = 'Curso de JavaScript'
@@ -10,3 +12,18 @@ nombreDelaVariableVar = 'maria';
 alert(nombreDelaVariableVar);
 
 console.log(NOMBREVARIABLE);
+*/
+
+let objeto = {
+  nombre: "David",
+  edad: "40"
+}
+
+let colores = ["Blanco", "Negro", "Azul"];
+
+console.log(objeto);
+console.log(colores);
+
+objeto.correo = "amhedriel@gmail.com";
+colores.push("Dorado");
+
